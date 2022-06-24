8bba7e44df52f7039831f8f0d9a1a32fdfe7d7f9
Doina Colun <doinacolun@macpro.local>
2022-06-24 11:15:42 -0700

    Added use effect and get list of tasks

--- src/App.js
+++ src/App.js
@@ -2,25 +2,62 @@ import React from 'react';
 import TaskList from './components/TaskList.js';
 import './App.css';
 import { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const TASKSLIST = [
-  {
-    id: 1,
-    title: 'Mow the lawn',
-    isComplete: false,
-  },
-  {
-    id: 2,
-    title: 'Cook Pasta',
-    isComplete: true,
-  },
-];
+// const TASKSLIST = [
+//   {
+//     id: 1,
+//     title: 'Mow the lawn',
+//     isComplete: false,
+//   },
+//   {
+//     id: 2,
+//     title: 'Cook Pasta',
+//     isComplete: true,
+//   },
+// ];
 
 const App = () => {
-  const [tasks, setTasks] = useState(TASKSLIST);
+  const [tasks, setTasks] = useState([]);
+  const url = 'https://task-list-api-c17.herokuapp.com/tasks';
+
+  useEffect(() => {
+    axios.get(url)
+      .then((response) => {
+        console.log(response);
+        const pulledTasks = response.data.map((task) => {
+          return {
+            id: task.id,
+            title: task.title,
+            isComplete: task.is_complete,
+            description: task.description,
+          };
+        });
+        console.log('in axios');
+        setTasks(pulledTasks);
+      })
+      .catch((error) => {console.log(error);});
+  }, []);
 
   const updatingTasks = (id) => {
     console.log('in arrow', id);
+
+    const updatedTasks = [...tasks];
+    let targetTask;
+    let completeness;
+    for (let task of updatedTasks) {
+      if (task.id === id) {
+        targetTask = task;
+        if (targetTask.isComplete) {
+          completeness = 'mark_incomplete';
+        } else {
+          completeness = 'mark_complete';
+        }
+      } 
+    }
+    axios.patch(`https://task-list-api-c17.herokuapp.com/tasks/${targetTask.id}/${completeness}`)
+         .then((response))
+
     const updatedTasks = [];
 
     for (const task of tasks) {
@@ -34,15 +71,26 @@ const App = () => {
   };
 
   const deleteTasks = (id) => {
-    const updatedTasks = [];
-    for (const task of tasks) {
-      if (task.id !== id) {
-        updatedTasks.push(task);
-      }
-      setTasks(updatedTasks);
-      console.log('in delete function');
-    }
+    // const updatedTasks = [];
+    // for (const task of tasks) {
+    //   if (task.id !== id) {
+    //     updatedTasks.push(task);
+    //   }
+    //   setTasks(updatedTasks);
+    //   console.log('in delete function');
+    // }
+    
+    axios.delete(`url/${id}`)
+          .then((response) => {
+            const updatedTasks = tasks.filter((task) => task.id !== id);
+            setTasks(updatedTasks);  
+          })
+          .catch((error) => {
+            console.log('Unable to delete');
+          });
   };
+
+
 
   return (
     <div className="App">
